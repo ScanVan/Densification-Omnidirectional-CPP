d0f87980868d01e6cf72a7e51bcad3b4f09d6132
Marcelo Kaihara <marcelo.kaihara@gmail.com>
2019-02-20 17:16:48 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Marcelo E. Kaihara
+// Version     :
+// Copyright   :
+// Description : Transforms the equirectangular image by applying rotations
+//============================================================================
+
+#include <iostream>
+
