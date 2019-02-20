bdcad07758648c6af276610527c9612266daf612
Marcelo Kaihara <marcelo.kaihara@gmail.com>
2019-02-20 17:33:10 +0100

    Load and display equirectangular images

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,38 @@
 
 #include <iostream>
 
+#include <opencv4/opencv2/core/types.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/features2d.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui/highgui.hpp>
+
+int main() {
+
+	cv::Mat image1{};
+	image1 = cv::imread("./data_in/equi1.bmp");
+	cv::Mat image2{};
+	image2 = cv::imread("./data_in/equi2.bmp");
+
+	if (!image1.data) {
+		std::cout << " Could not open or find the image" << std::endl;
+		return -1;
+	}
+	if (!image2.data) {
+		std::cout << " Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+
+	cv::namedWindow("Display window1", cv::WINDOW_KEEPRATIO);
+	cv::imshow ("Display window1", image1);
+	cv::namedWindow("Display window2", cv::WINDOW_KEEPRATIO);
+	cv::imshow ("Display window2", image2);
+	cv::waitKey(0);
+
+
+
+	return 0;
+
+}
